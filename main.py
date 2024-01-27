532b59b49d1df5622881a08e3e5b46764b8c693c
Murphy <zacharymjameson@gmail.com>
2024-01-27 15:34:38 -0500

    snake movement

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 import pygame
 from snake import *
+from food import Food
 
 pygame.init()
 bounds = (300, 300)
 window = pygame.display.set_mode(bounds)
 pygame.display.set_caption("Snake")
 block_size = 20
+
 snake = Snake(block_size, bounds)
+food = Food(block_size, bounds)
+
 clock = pygame.time.Clock()
 
 is_running = True
 while is_running:
-    pygame.time.delay(100)
+    pygame.time.delay(120)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        snake.steer(Direction.LEFT)
+    elif keys[pygame.K_RIGHT]:
+        snake.steer(Direction.RIGHT)
+    elif keys[pygame.K_UP]:
+        snake.steer(Direction.UP)
+    elif keys[pygame.K_DOWN]:
+        snake.steer(Direction.DOWN)
+
+    snake.move()
     window.fill((0, 0, 0))
     snake.draw(pygame, window)
+    food.draw(pygame, window)
     pygame.display.flip()
     clock.tick(10)
--- snake.py
+++ snake.py
@@ -30,3 +30,31 @@ class Snake:
         for segment in self.body:
             game.draw.rect(
                 window, self.color, (segment[0], segment[1], self.block_size, self.block_size))
+
+    def move(self):
+        curr_head = self.body[-1]
+        if self.direction == Direction.DOWN:
+            next_head = (curr_head[0], curr_head[1] + self.block_size)
+            self.body.append(next_head)
+        elif self.direction == Direction.UP:
+            next_head = (curr_head[0], curr_head[1] - self.block_size)
+            self.body.append(next_head)
+        elif self.direction == Direction.RIGHT:
+            next_head = (curr_head[0] + self.block_size, curr_head[1])
+            self.body.append(next_head)
+        elif self.direction == Direction.LEFT:
+            next_head = (curr_head[0] - self.block_size, curr_head[1])
+            self.body.append(next_head)
+
+        if self.length < len(self.body):
+            self.body.pop(0)
+
+    def steer(self, direction):
+        if self.direction == Direction.DOWN and direction != Direction.UP:
+            self.direction = direction
+        elif self.direction == Direction.UP and direction != Direction.DOWN:
+            self.direction = direction
+        elif self.direction == Direction.LEFT and direction != Direction.RIGHT:
+            self.direction = direction
+        elif self.direction == Direction.RIGHT and direction != Direction.LEFT:
+            self.direction = direction
