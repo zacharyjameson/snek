c8d65c5451544f309200dcdd1be9377ca0cfe430
Murphy <zacharymjameson@gmail.com>
2024-01-27 13:58:35 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
+test_surface = pygame.image.load('images/backgrounds.png')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(test_surface, (200, 100))
+
+    pygame.display.update()
+    clock.tick(60)
